cc640b881c244870f8d138cf27e2657acf531fd3
QingWei <l.q.wei91@gmail.com>
2025-10-11 16:30:32 +0100

    Got basic code run

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+#![feature(sync_unsafe_cell)]
+#![feature(maybe_uninit_uninit_array)]
+mod exp;
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+
+}
